95ff55fb86abd1f17af8f06d7f1c1687fbc77abc
FENGWENHAO <evanfungv@gmail.com>
2018-02-13 01:00:21 +0800

    chore:change the material ui version to material ui next and delete all components

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import createHistory from "history/createBrowserHistory";
-import App from "./main/App";
-import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter } from "react-router-dom";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import "./res/styles/index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import './res/styles/index.css'
+import 'typeface-roboto'
 
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
 ReactDOM.render(
   <BrowserRouter>
-    <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </BrowserRouter>,
-  document.getElementById("root")
-);
-registerServiceWorker();
+  document.getElementById('root')
+)
+registerServiceWorker()
--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from "react";
-import "../res/styles/App.css";
-import { Route, Switch } from "react-router-dom";
-import HeaderBar from "./components/header";
-import PostList from "./components/posts/PostList";
-import RaisedButton from "material-ui/RaisedButton";
-import BottomNavigator from "./components/footer";
+import React, { Component } from 'react'
+import '../res/styles/App.css'
+import Reboot from 'material-ui/Reboot'
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <HeaderBar />
-        <PostList className="main" />
-        <BottomNavigator className="footer" />
+        <Reboot />
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
