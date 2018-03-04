0d4c2213f292b63a4b73410bd90ce72accd4abb2
evan fung <evanfungv@gmail.com>
2018-03-05 07:06:40 +0800

    feat:add a updatePostScore function to actions

--- client/src/main/actions/post.js
+++ client/src/main/actions/post.js
@@ -3,6 +3,9 @@ import * as PostsAPI from "../utils/PostsAPI";
 export const FETCH_POSTS = "FETCH_POSTS";
 export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS";
 export const FETCH_POSTS_FAILURE = "FETCH_POSTS_FAILURE";
+
+export const UPDATE_POST_SCORE = "UPDATE_POST_SCORE";
+
 export function fetchPostsSuccess(posts) {
   return {
     type: FETCH_POSTS_SUCCESS,
@@ -29,8 +32,33 @@ export function fetchPosts() {
   };
 }
 
-export function updatePostScore({post,voteType}) {
-  return dispatch => {
-    
+export function updatePostScore({
+  status = null,
+  response = null,
+  post,
+  voteType
+}) {
+  //if we fetched the data, return an action
+  if (status === "success" || statsu === "error") {
+    return {
+      type: UPDATE_POST_SCORE,
+      status,
+      response,
+      voteType
+    };
   }
+
+  //else using promise to fetch the data
+  return dispatch => {
+    PostsAPI.vote(post, voteType)
+      .then(post => {
+        console.log(`${post.id} has an score of ${post.voteScore}`);
+        //if successfully get the data, dispatch an action.
+        dispatch(updatePostScore({ status: "success", response: post }));
+      })
+      .catch(error => {
+        //if an error occured, dispatch an action.
+        dispatch(updatePostScore({ status: "error", response: error }));
+      });
+  };
 }
