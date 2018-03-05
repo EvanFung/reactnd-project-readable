335ae012aa17b1e685d78659cc5f4c9dd43b8ac2
evan fung <evanfungv@gmail.com>
2018-03-05 08:15:54 +0800

    feat:add a vote function

--- client/src/main/components/posts/button/UpDownVoter.js
+++ client/src/main/components/posts/button/UpDownVoter.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { CardActions } from 'material-ui/Card'
-import IconButton from 'material-ui/IconButton'
-import Typography from 'material-ui/Typography'
-import ThumbUp from 'material-ui-icons/ThumbUp'
-import ThumbDown from 'material-ui-icons/ThumbDown'
-import classnames from 'classnames'
-import styles from '../../../styles/post/button/UpDownVoter'
+import React from "react";
+import { CardActions } from "material-ui/Card";
+import IconButton from "material-ui/IconButton";
+import Typography from "material-ui/Typography";
+import ThumbUp from "material-ui-icons/ThumbUp";
+import ThumbDown from "material-ui-icons/ThumbDown";
+import classnames from "classnames";
+import styles from "../../../styles/post/button/UpDownVoter";
 class UpDownVoter extends React.Component {
-  constructor(props) {
-    super(props)
+  state = {
+    voteScore:this.props.post.voteScore
   }
 
+  handleUpdateScore = () => {
+    this.setState({voteScore: this.props.post.voteScore})
+  }
+
+
   render() {
-    const { classes } = this.props
+    const { classes, post, updatePostScore } = this.props;
     return (
       <div>
         <CardActions disableActionSpacing className={classes.root}>
-          <IconButton aria-label="Add to favorites">
+          <IconButton
+            aria-label="Add to favorites"
+            onClick={() => {
+              updatePostScore(post, "upVote");
+              this.handleUpdateScore();
+            }}
+          >
             <ThumbUp />
           </IconButton>
-          <Typography>{this.props.post.voteScore}</Typography>
-          <IconButton aria-label="Add to favorites">
+          <Typography>{this.state.voteScore}</Typography>
+          <IconButton
+            aria-label="Add to favorites"
+            onClick={() => {
+              updatePostScore(post, "downVote");
+              this.handleUpdateScore();
+            }}
+          >
             <ThumbDown />
           </IconButton>
         </CardActions>
       </div>
-    )
+    );
   }
 }
 
-export default styles(UpDownVoter)
+export default styles(UpDownVoter);
--- client/src/main/components/posts/list/PostCardItem.js
+++ client/src/main/components/posts/list/PostCardItem.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import Card from 'material-ui/Card'
-import PostContent from '../body/PostContent'
-import styles from '../../../styles/post/PostCardItem'
-import UpDownVoter from '../button/UpDownVoter'
-import CommentButton from '../button/CommentButton'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Card from "material-ui/Card";
+import PostContent from "../body/PostContent";
+import styles from "../../../styles/post/PostCardItem";
+import UpDownVoter from "../button/UpDownVoter";
+import CommentButton from "../button/CommentButton";
+import { connect } from "react-redux";
+import * as PostActions from "../../../actions/post";
 class PostCardItem extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
-    const { classes } = this.props
+    const { classes,actions } = this.props;
     return (
       <div>
         <Card className={classes.root}>
           <PostContent post={this.props.post} />
           <div className={classes.footer}>
-            <UpDownVoter post={this.props.post}/>
+            <UpDownVoter post={this.props.post} updatePostScore={actions.updatePostScore} />
             <CommentButton post={this.props.post} />
           </div>
         </Card>
       </div>
-    )
+    );
   }
 }
-export default styles(PostCardItem)
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: {
+      updatePostScore: (post, voteType) =>
+        dispatch(PostActions.updatePostScore({ post, voteType }))
+    }
+  };
+};
+export default connect(null,mapDispatchToProps)(styles(PostCardItem));
