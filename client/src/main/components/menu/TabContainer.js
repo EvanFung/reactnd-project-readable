3630a571502bae71be2a66e90c5df06c5c4bc1ce
evan fung <evanfungv@gmail.com>
2018-03-09 10:51:37 +0800

    feat: add a sort post by timestamp and voteScore feature.

--- client/src/main/components/menu/TabContainer.js
+++ client/src/main/components/menu/TabContainer.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Tabs, { Tab } from 'material-ui/Tabs'
-import styles from '../../styles/menu/TabContainer'
+import React from "react";
+import Tabs, { Tab } from "material-ui/Tabs";
+import styles from "../../styles/menu/TabContainer";
+import { connect } from "react-redux";
+import * as PostActions from "../../actions/post";
 class TabContainer extends React.Component {
   state = {
     value: 0
-  }
+  };
   handleChange = (event, value) => {
-    this.setState({ value })
-  }
+    this.setState({ value });
+  };
   render() {
+    
     return (
       <div>
         <Tabs
@@ -19,12 +22,28 @@ class TabContainer extends React.Component {
           value={this.state.value}
           onChange={this.handleChange}
         >
-          <Tab label="NEW" />
-          <Tab label="HOT" />
+          <Tab
+            label="NEW"
+            onClick={() => {
+              this.props.actions.sortPostsBy("timestamp");
+            }}
+          />
+          <Tab
+            label="HOT"
+            onClick={() => {
+              this.props.actions.sortPostsBy("voteScore");
+            }}
+          />
         </Tabs>
       </div>
-    )
+    );
   }
 }
-
-export default styles(TabContainer)
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      sortPostsBy: criteria => dispatch(PostActions.sortPostsBy({ criteria }))
+    }
+  };
+}
+export default connect(null,mapDispatchToProps)(styles(TabContainer));
