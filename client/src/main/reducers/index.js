dfd00cc5b9e0088ed5e4952fb8783efc113b8779
evan fung <evanfungv@gmail.com>
2018-03-09 11:32:25 +0800

    feat: add a category action, api, and reducer

--- client/src/main/reducers/index.js
+++ client/src/main/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 import { posts } from "./post";
+import { activeCategory,categories } from './category'
 
 export default combineReducers({
-  posts:posts
+  posts,
+  activeCategory,
+  categories
 });
--- client/src/main/utils/PostsAPI.js
+++ client/src/main/utils/PostsAPI.js
@@ -81,3 +81,7 @@ export const createPost = ({ title, body, author, category }) => {
     body: JSON.stringify(data)
   }).then(res => res.json());
 };
+
+export const getCategories = () => {
+  return fetch(`${api}/categories`, { headers }).then(res => res.json());
+};
