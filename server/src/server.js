be0b4adeb1c567207bc7817b82678451d616bba7
evan fung <evanfungv@gmail.com>
2018-03-12 13:53:09 +0800

    chore: deploy the client and server side

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+//https://reddit-clone-server-pczpjmmglv.now.sh
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
